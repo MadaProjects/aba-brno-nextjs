3bfd81929ff1046dd8f3d7e5fae88289fa2cb14a
Peter Mada <petermada@gmail.com>
2022-12-18 18:37:52 +0100

    Add tailwind form

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import { ConatactForm } from '../Form/ContactForm';
 
 export const Tabs = ({ tabs }) => {
   const [currentTab, setCurrentTab] = useState(0);
-  const [maxTabs, setMaxTabs] = useState(0);
+  const [maxTabs, setMaxTabs] = useState(tabs.length + 1);
 
   useEffect(() => {
-    setMaxTabs(tabs.length);
-  }, []);
+    //setMaxTabs(tabs.length + 1);
+  }, [tabs]);
 
   const handleTabChange = (e) => {
-    console.log(`TAB CHANGED  - ${maxTabs}`);
     setCurrentTab(e.currentTarget.dataset.id);
   };
 
@@ -59,6 +59,24 @@ export const Tabs = ({ tabs }) => {
               </button>
             ))
           : ''}
+
+        <button
+          data-id={999}
+          id={`tab-999`}
+          role='tab'
+          aria-selected={currentTab === 999 ? true : false}
+          aria-controls={`tabpanel-999`}
+          key={`tabbtn-999`}
+          tabIndex={currentTab == 999 ? '0' : '-1'}
+          onClick={handleTabChange}
+          onKeyDown={changeTabOnKeyDown}
+          className={
+            currentTab == 999
+              ? 'inline-block p-4 rounded-t-lg border-b-2 text-primary hover:text-blue-600 dark:text-secondary dark:hover:text-blue-500 border-primary dark:border-secondary'
+              : 'inline-block p-4 rounded-t-lg border-b-2 hover:text-blue-600 dark:hover:text-blue-500'
+          }>
+          <span className='inline'>Kontakt</span>
+        </button>
       </div>
       <div>
         {tabs
@@ -77,6 +95,18 @@ export const Tabs = ({ tabs }) => {
               </div>
             ))
           : ''}
+        <div
+          key={`tabpanel-999`}
+          id={`tabpanel-999`}
+          tabIndex='0'
+          aria-labelledby={`tab-999`}
+          className={
+            currentTab == 999
+              ? 'block p-4 bg-gray-50 rounded-lg dark:bg-gray-800'
+              : 'hidden'
+          }>
+          <ConatactForm />
+        </div>
       </div>
     </div>
   );
