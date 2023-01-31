00fee3299c07812daed317abbdb50fd36b39c7d9
Peter Mada <petermada@gmail.com>
2023-01-31 17:14:22 +0100

    Update for netlify

--- appolo-client.js
+++ appolo-client.js
@@ -3,7 +3,10 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 // TODO add uri to env
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:1337/graphql', fetch }),
+  link: new HttpLink({
+    uri: 'https://aba-brno-strapi.herokuapp.com/graphql',
+    fetch,
+  }),
   //uri: 'http://localhost:1337/graphql',
   cache: new InMemoryCache(),
 });
--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,7 @@
 import '@testing-library/jest-dom';
 // TODO should be in api-mocks folder
 import { mswServer } from './helpers/tests/api-mocks/msw-server';
-process.env.NEXT_PUBLIC_API_URL = 'http://localhost:1337';
+process.env.NEXT_PUBLIC_API_URL = 'https://aba-brno-strapi.herokuapp.com';
 
 beforeAll(() => mswServer.listen());
 afterEach(() => mswServer.resetHandlers());
